930cad3a2c12ab2863a617abb0b4fd5346095c5c
emirosman <m.emirosman@gmail.com>
2016-12-10 09:39:20 +0200

    Add files via upload

--- DCclient.c
+++ DCclient.c
@@ -4,19 +4,63 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <errno.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <pthread.h>
 
-#define SUNUCUPORT 2019
+#define SUNUCUPORT 2015
 #define SUNUCUIP "127.0.0.1"
 #define AZAMIUZUNLUK 1024
 
-int main()
-{
     int sockfd; //soket oluşturulcak değişken
     struct sockaddr_in serverAddr;// ?? sockaddr_in adında struct var ondan serveraddr diye nesne mi oluşturdum ??
-    char *gonder="mesaj1";//göndericeğimiz mesaj değişkeni
+    char gonder[30];//AZAMIUZUNLUK ile değişsin
     char str[AZAMIUZUNLUK];//array for save coming msg
     int gelenBayt,gidenBayt,structSize;
-    //değişkenler bitti
+    //değişkenler global olarak tanımlandı ki fonksiyon parametreleriyle uğraştırmasın
+
+void *yaz()
+{
+while(1)
+{
+    printf("\nmsj:"); scanf("%s",gonder);
+    gidenBayt=send(sockfd,gonder,strlen(gonder),0);
+    if(-1==gidenBayt)
+    {perror("send hata");}
+    else if (strlen(gonder) != gidenBayt)
+    {printf("gönderilen giden eşit değil");}
+    
+ }
+}
+
+
+void *oku()
+{   
+while(1)
+{
+    memset(str,0,AZAMIUZUNLUK*sizeof str[0]);
+    gelenBayt=recv(sockfd,&str,AZAMIUZUNLUK-1,0);
+    if(-1==gelenBayt)
+    {perror("recv hata");}
+    else if (0==gelenBayt)
+    {printf("bağlantı koptu");break;}
+    else
+    {
+        /*if(str[0]=="8")//çıkış tuşu konmalı ?
+        {break;}
+        else*/
+        {printf("msj alındı: %s \n",str);}
+    }
+}
+}
+
+
+int main()
+{
+    pthread_t thread[2];
+    int tired1,tired2;
     
     sockfd=socket(AF_INET,SOCK_STREAM,0);//soket oluşturuldu sockfd de tutuldu
     if(-1==sockfd)//hata olursa sockfd ye -1 döner
@@ -31,25 +75,11 @@ int main()
     if(-1==connect(sockfd,(struct sockaddr *)&serverAddr,sizeof (struct sockaddr)))
     {perror("'connect' bağlantı kurmada hata!");}
     //sunucuyla bağlantı kuruldu
-    int i=0;
-    for(i;i<5;i++)
-    {
-    gidenBayt=send(sockfd,gonder,strlen(gonder),0);
-    if(-1==gidenBayt)
-    {perror("mesaj göndermede hata!");}
-    else if(strlen(gonder) != gidenBayt)
-    {printf("mesaj eksik gitti!");}
-    else 
-    {printf("mesaj gönderildi\n");}
-    memset(str,'\0',30);//hafızanın temizlenmesi gerekiyor(yeri değişebilir kontrol et)
-    gelenBayt=recv(sockfd,&str,AZAMIUZUNLUK-1,0);
-    if(-1==gelenBayt)
-    {perror("mesaj almada hata!");}
-    else if(0==gelenBayt)
-    {printf("bağlantı koptu");}
-    else 
-    {printf("mesaj alındı: %s \n",str);}
-    }
+    
+    tired1=pthread_create(thread+0,NULL,yaz,NULL);
+    tired2=pthread_create(thread+1,NULL,oku,NULL);
+    
+    pthread_exit(NULL);
     close(sockfd);
 }
 
--- DCserver.c
+++ DCserver.c
@@ -4,19 +4,60 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <errno.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <pthread.h>
 
-#define LISTENPORT 2019
+#define LISTENPORT 2015
 #define AZAMIUZUNLUK 1024
 
-int main()
-{
     int sockfd,newfd;
     struct sockaddr_in serverAddr;
     struct sockaddr_in clientAddr;
-    char gonder[30]="server msj1";
+    char gonder[30];
     char str[AZAMIUZUNLUK];
     int gelenBayt,gidenBayt,structSize;
-    //değişkenler tanımlandı
+    //fonksiyonlara parametre göndermemek için değişkenler global tanımlandı
+
+void *oku()
+{
+while(1)
+{
+    memset(str,0,AZAMIUZUNLUK*sizeof str[0]);
+    gelenBayt=recv(newfd,&str,AZAMIUZUNLUK-1,0);
+    if(-1==gelenBayt)
+    {perror("msj okunamadı:recv!");}
+    else if(0==gelenBayt)
+    {perror("bağlantı koptu");break;}
+    else
+    {
+        /*if(str[0]=="8")//8 çıkış tuşu konmalı
+        {break;}
+        else*/
+        //{printf("%d Bayt aldım: %s",gelenBayt,str);} //bu şekilde gelen mesajı mesaj yolladıktan sonra gösteriyo neden ??
+        {printf("msj alındı: %s \n",str);}
+    }
+}
+}
+void *yaz()
+{
+while(1)
+{
+    printf("\nmsj:");scanf("%s",gonder);
+    gidenBayt=send(newfd,gonder,strlen(gonder),0);
+    if(gidenBayt==-1)
+    {perror("msj yollanamadı:send!\n");}
+    else if(strlen(gonder) != gidenBayt)
+    {printf("gönderilen giden eşit değil:send! \n");}
+ }  
+}
+
+
+int main()
+{
+    pthread_t thread[2];
+    int tired1,tired2;
     
     sockfd=socket(AF_INET,SOCK_STREAM,0);
     if(-1==sockfd)
@@ -36,31 +77,16 @@ int main()
     {perror("listen hata!");}
     //soketi listen a alındı bağlantı kabul edilicek
     structSize=sizeof(clientAddr);
+    
     newfd=accept(sockfd,(struct sockaddr *)&clientAddr,&structSize);
     if(-1==newfd)
     {perror("accept hata");}
     //bağlantı kuruldu mesaj okunucak
-    int i=0;
-    for(i;i<5;i++)
-    {
-    gelenBayt=recv(newfd,&str,AZAMIUZUNLUK-1,0);
-    if(-1==gelenBayt)
-    {perror("recv hata!");}
-    else if(0==gelenBayt)
-    {printf("bağlantı koptu");}
-    else
-    {printf("%d bayt aldım mesaj: %s \n",gelenBayt,str);}
-    //mesaj alındı soketi send e alıp mesaj yollanıcak
-    memset(gonder,'\0',30);//hafızanın temizlenmesi gerekiyor
-    printf("mesaj gir:");scanf("%s",gonder);
-    gidenBayt=send(newfd,gonder,strlen(gonder),0);
-    if(-1==gidenBayt)
-    {perror("send hata!");}
-    else if(strlen(gonder) != gidenBayt)
-    {printf("mesaj eksik gönderildi!");}
-    else
-    {printf("mesaj gönderildi:%s \n",gonder);}
-    }
+    
+    tired1=pthread_create(thread+0,NULL,yaz,NULL);
+    tired2=pthread_create(thread+1,NULL,oku,NULL);
+
+    pthread_exit(NULL);
     close(newfd);
     close(sockfd);
     
